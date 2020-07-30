cdb930672e6360c2ed15249902f0827fe818f3b9
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-07-30 23:22:29 +0300

    refactoring diaries

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -1,16 +1,26 @@
 import diaries  from '../../data/diaries';
 
-import { DiaryEntry } from '../types';
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
 
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
 };
 
-const addEntry = () => {
-  return null;
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
+};
+
+const addDiary = () => {
+  return [];
 };
 
 export default {
   getEntries,
-  addEntry
+  getNonSensitiveEntries,
+  addDiary
 };
